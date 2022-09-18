84f54d2d7cb606baaa86629b44075ce2596ff706
weslley-lima24 <weslley.lima24@etec.sp.gov.br>
2022-09-17 23:08:02 -0300

    Cadastro e Login funcionando

---
+++ app/src/main/java/com/teste/apiyoutube/Login.java
@@ -0,0 +1,133 @@
+package com.teste.apiyoutube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+public class Login extends AppCompatActivity
+{
+
+
+    TextInputEditText inputUser, inputSenha;
+    Button btnLogin;
+    TextView txtLogin;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        inputUser = findViewById(R.id.edtLoginUsuario);
+        inputSenha = findViewById(R.id.edtLoginSenha);
+        btnLogin = findViewById(R.id.btnLogin);
+        txtLogin = findViewById(R.id.txtLogin);
+        progressBar = findViewById(R.id.progressLogin);
+
+
+        txtLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String username;
+                String password;
+                username = String.valueOf(inputUser.getText());
+                password = String.valueOf(inputSenha.getText());
+                if (checarVazio(username, password))
+                {
+                    progressBar.setVisibility(View.VISIBLE);
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[2];
+
+                            field[0] = "username";
+                            field[1] = "password";
+
+                            //Creating array for data
+                            String[] data = new String[2];
+
+                            data[0] = username;
+                            data[1] = password;
+
+                            PutData putData = new PutData("http://192.168.1.3/api_youtube/login.php", "POST", field, data);
+                            if (putData.startPut())
+                            {
+                                if (putData.onComplete())
+                                {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    //End ProgressBar (Set visibility to GONE)
+                                    if(result.equals("Login realizado com sucesso!"))
+                                    {
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                        Toast.makeText(getApplicationContext(), "Login realizado", Toast.LENGTH_LONG).show();
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                                    }
+
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    });
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Todos os campos precisam ser preenchidos", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
+
+
+
+    private boolean checarVazio(String a, String b)
+    {
+        if (!a.equals("") && !b.equals(""))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/teste/apiyoutube/SignUp.java
@@ -0,0 +1,143 @@
+package com.teste.apiyoutube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+public class SignUp extends AppCompatActivity
+{
+
+    TextInputEditText inputFullname, inputEmail, inputUser, inputSenha;
+    Button btnCadastro;
+    TextView txtCadastro;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        inputFullname = findViewById(R.id.edtFullname);
+        inputEmail = findViewById(R.id.edtEmail);
+        inputUser = findViewById(R.id.edtUsername);
+        inputSenha = findViewById(R.id.edtPassword);
+
+        btnCadastro = findViewById(R.id.btnCadastro);
+        txtCadastro = findViewById(R.id.txtCadastro);
+        progressBar = findViewById(R.id.progress);
+
+        txtCadastro.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        btnCadastro.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String fullname, email, username, password;
+
+                fullname = String.valueOf(inputFullname.getText());
+                email = String.valueOf(inputEmail.getText());
+                username = String.valueOf(inputUser.getText());
+                password = String.valueOf(inputSenha.getText());
+
+
+
+                if (checarVazio(fullname, email, username, password))
+                {
+                    progressBar.setVisibility(View.VISIBLE);
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[4];
+                            field[0] = "fullname";
+                            field[1] = "username";
+                            field[2] = "password";
+                            field[3] = "email";
+                            //Creating array for data
+                            String[] data = new String[4];
+                            data[0] = fullname;
+                            data[1] = username;
+                            data[2] = password;
+                            data[3] = email;
+                            PutData putData = new PutData("http://192.168.1.3/api_youtube/signup.php", "POST", field, data);
+                            if (putData.startPut())
+                            {
+                                if (putData.onComplete())
+                                {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    //End ProgressBar (Set visibility to GONE)
+                                    if(result.equals("Cadastro realizado com sucesso!"))
+                                    {
+                                        Intent intent = new Intent(getApplicationContext(), Login.class);
+                                        startActivity(intent);
+                                        finish();
+                                        Toast.makeText(getApplicationContext(), "Cadastro realizado", Toast.LENGTH_LONG).show();
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                                    }
+
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    });
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Todos os campos precisam ser preenchidos", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+
+
+
+
+    }
+
+    private boolean checarVazio(String a, String b, String c, String d)
+    {
+        if (!a.equals("") && !b.equals("") && !c.equals("") && !d.equals(""))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+}
